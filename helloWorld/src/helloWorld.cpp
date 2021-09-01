1f5cd378fae1342514678d992bb9296a0426c9ac
yajunli01 <jason.li@igg.com>
2021-09-01 13:36:25 +0800

    + add VersionNumber and Configured header file, Specify C++ Standard to C++ 11

--- helloWorld/src/helloWorld.cpp
+++ helloWorld/src/helloWorld.cpp
@@ -1,14 +1,38 @@
 #include <iostream>
 #include <string>
+#include <cmath>
+
+// this header file gen by CMakeLists::configure_file(helloWorld.h.in src/helloWorld.h)
+#include "helloWorld.h"
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
 	cout << "Welcome to CMake!" << endl;
+	
+	cout << argv[0] << " Version:" << helloWorld_VERSION_MAJOR << "." << helloWorld_VERSION_MINOR << endl;
+	
 	cout << "Hello, World!" << endl;
-	cout << "输入任意字符退出!" << endl;
-	string temp;
-	cin >> temp;
+	
+	
+	if(argc < 2)
+	{
+		cout << "Usage: " << argv[0] << " Number" << endl;
+		//return -1;
+		cout << "输入一个数字用于求它的平方根:" << endl;
+		string inputValue;
+		cin >> inputValue;
+		const double input = std::stod(inputValue);
+		const double output = sqrt(input);
+		cout << "The square root of " << input << " is " << output << endl;
+	}
+	else
+	{
+		const double input = std::stod(argv[1]);
+		const double output = sqrt(input);
+		cout << "The square root of " << input << " is " << output << endl;
+	}
+	
 	return 0;
 }
---
+++ helloWorld/src/helloWorld.h
@@ -0,0 +1,3 @@
+//注意: CMakeLists.txt里定义的ProjectName是什么,这里定义的版本号前面也要保持完全一致！包括大小写！
+#define helloWorld_VERSION_MAJOR 1
+#define helloWorld_VERSION_MINOR 0
