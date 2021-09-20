c70157822fe840cd3492fc29cdd0e381966a670d
kingmax <kingmax_res@163.com>
2021-09-20 14:33:31 +0800

    * 3 static library(first build lib, the build exe using the lib)

--- 00/3_static_lib/build.bat
+++ 00/3_static_lib/build.bat
@@ -2,5 +2,5 @@ md build
 cd build
 cmake -G "Visual Studio 16 2019" -A x64 ..
 REM cmake --build . --config release --verbose
-cmake --build . --config release
+cmake --build . --config release -v
 cd ..
