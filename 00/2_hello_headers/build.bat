5802353f2cea6aea60484d7151cc2afe099d624d
kingmax <kingmax_res@163.com>
2021-09-20 12:35:20 +0800

    + 2 hello_headers (split header, source code)

---
+++ 00/2_hello_headers/build.bat
@@ -0,0 +1,5 @@
+md build
+cd build
+cmake -G "Visual Studio 16 2019" -A x64 ..
+cmake --build . --config release
+cd ..
---
+++ 00/2_hello_headers/src/main.cpp
@@ -0,0 +1,9 @@
+#include "hello.h"
+//#include <iostream>
+int main(int argc, char* argv[])
+{
+	Hello hi;
+	hi.print();
+	//getchar();
+	return 0;
+}
